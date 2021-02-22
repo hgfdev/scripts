8e3d576dc5c23840174f7e48f1c5f01274fa870d
hgfdev <59458374+hgfdev@users.noreply.github.com>
2021-02-22 16:02:14 -0500

    Update 0-run-hhl-backup.bat

--- windows/hhl-backup/0-run-hhl-backup.bat
+++ windows/hhl-backup/0-run-hhl-backup.bat
@@ -1,2 +1,2 @@
 #Used to execute a powershell script without enabling script execution on a system which may be vulnerability. 
-PowerShell -NoProfile -ExecutionPolicy Unrestricted -Command .\hhl-backup.ps1
+PowerShell -NoProfile -ExecutionPolicy Unrestricted -Command .\1-hhl-backup.ps1
