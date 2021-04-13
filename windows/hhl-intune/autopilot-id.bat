8bf67452a2cb24ada66db8cf20f88e18632de841
hgfdev <59458374+hgfdev@users.noreply.github.com>
2021-04-13 10:10:15 -0400

    Create autopilot-id.bat

---
+++ windows/hhl-intune/autopilot-id.bat
@@ -0,0 +1,1 @@
+PowerShell -NoProfile -ExecutionPolicy Unrestricted -Command D:\PreAutoPilot\Get-WindowsAutoPilotInfo.ps1 -ComputerName $env:computername -OutputFile D:\PreAutoPilot\computers.csv -append
